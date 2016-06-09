9b72c30650c35f45d6f27ca335802113e6a2b87e
rabidgremlin <jack@rabidgremlin.com>
2016-06-09 13:58:36 +1200

    Added real script.

--- hugo-s3.sh
+++ hugo-s3.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo "Hello"
-echo '$0 = ' $0
-echo '$1 = ' $1
+if [ $# -lt 4 ] ; then
+  echo "Usage:"
+  echo "      docker run rabidgremlin/hugo-s3 <git url> <s3 bucket name> <AWS access key> <AWS secret key>"
+  exit 1;
+fi
 
+git clone $1 site
+cd site
+hugo
+cd public
+
+export AWS_ACCESS_KEY_ID=$3
+export AWS_SECRET_ACCESS_KEY=$4
+
+aws s3 sync . s3://$2/ --delete
